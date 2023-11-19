62972baf84c0197cdfd129d5c3db46c2cb44171c
WhyitRiot <wyatt.errp@gmai.com>
2023-11-19 12:17:35 +0900

    Added icons to taxi screen

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # SnakeEyesConnect
 
 1. Install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
 2. nvm install 14
 3. nvm use 14
 4. npm install expo
 5. git clone
 6. cd into directory
-7. npm install @react-navigation/native
-8. npm install @react-navigation/native-stack
-9. npx expo install react-native-screens react-native-safe-area-context
+7. npm install @react-navigation/native @react-navigation/native-stack @expo/vector-icons
+8. npx expo install react-native-screens react-native-safe-area-context
--- src/screens/Taxi.js
+++ src/screens/Taxi.js
@@ -1,30 +1,47 @@
 import React from 'react'
 import {StyleSheet, Text, View, FlatList, Linking, TouchableOpacity} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
+import {AntDesign, FontAwesome, MaterialCommunityIcons} from '@expo/vector-icons'
 
 const data = [
     {
         id: '00',
         title: 'Daiko Taxi',
-        number: '098-932-4035'
+        number: '098-932-4035',
+        icon: () => {
+            return(
+                <FontAwesome name="taxi" size={24} color="black"/>
+            )
+        }
     },
     {
         id: '01',
         title: 'Panda Taxi',
-        number: '098-970-8888'
+        number: '098-970-8888',
+        icon: () => {
+            return(
+                <MaterialCommunityIcons name="panda" size={24} color="black"/>
+            )
+        }
     }
 ]
 
 const Item = ({item, onPress, backgroundColor, textColor}) => (
-    <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
-        <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
+    <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>  
+        <View style={styles.itemContent}>
+            <View style={styles.leftContent}>
+                <item.icon />
+                <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
+            </View>
+            <AntDesign name="phone" size={24} color="black"/>
+        </View>
     </TouchableOpacity>
 )
 
 const Taxi = () => {
     const renderItem = ({item}) => {
-        const backgroundColor = 'black' 
-        const color = 'white'
+        const backgroundColor = 'white' 
+        const color = 'black'
         return (
             <Item
                 item={item}
@@ -34,12 +51,18 @@ const Taxi = () => {
             />
         )
     }
+    const renderSeparator = () => (
+        <View style={{backgroundColor: 'black', height: 2}} />
+    );
     return(
-        <SafeAreaView style={{backgroundColor: 'black'}}>
+        <SafeAreaView edges={['right','bottom','left']} style={styles.container}>
             <FlatList 
                 data={data}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
+                ItemSeparatorComponent={renderSeparator}
+                ListHeaderComponent={renderSeparator}
+                ListFooterComponent={renderSeparator}
             />
         </SafeAreaView>
     )
@@ -49,13 +72,24 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         flexDirection: 'row',
-        alignItems: 'space-around'
+        backgroundColor: 'gray',
     },
     item:{
-
+        padding: 30,
+        alignContent: 'space-around'
+    },
+    itemContent:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    leftContent:{
+        flexDirection: 'row',
+        alignItems: 'center'
     },
     title:{
-        fontSize: 20
+        marginLeft: 25,
+        fontSize: 20,
     }
 })
 export default Taxi;
